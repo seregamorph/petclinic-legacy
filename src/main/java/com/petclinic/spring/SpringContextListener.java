2f633fbc1b9eb5b98a0e0f724686de4ee2cdaba5
seregamorph <serega.morph@gmail.com>
2026-04-04 16:00:50 +0200

    log failure

--- src/main/java/com/petclinic/spring/SpringContextListener.java
+++ src/main/java/com/petclinic/spring/SpringContextListener.java
@@ -3,10 +3,14 @@ package com.petclinic.spring;
 import com.petclinic.PetclinicApplication;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class SpringContextListener implements ServletContextListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringContextListener.class);
 
     private ConfigurableApplicationContext applicationContext;
 
@@ -14,7 +18,12 @@ public class SpringContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         var springApplication = new SpringApplication(PetclinicApplication.class);
         String[] args = {};
-        applicationContext = springApplication.run(args);
+        try {
+            applicationContext = springApplication.run(args);
+        } catch (Exception e) {
+            logger.error("Error while loading spring context", e);
+            throw e;
+        }
     }
 
     @Override
