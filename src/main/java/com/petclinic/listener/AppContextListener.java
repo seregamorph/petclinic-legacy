445851cdb3e0c1e8789074e8720f5a7120dd0e33
seregamorph <serega.morph@gmail.com>
2026-04-04 16:24:12 +0200

    Get rid of web.xml

--- src/main/java/com/petclinic/listener/AppContextListener.java
+++ src/main/java/com/petclinic/listener/AppContextListener.java
@@ -1,19 +1,20 @@
 package com.petclinic.listener;
 
 import com.petclinic.config.DatabaseConfig;
-
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * Runs on application startup: executes Flyway migrations
  * and eagerly initialises all singletons so the first request
  * doesn't pay the initialisation cost.
  */
-public class AppContextListener implements ServletContextListener {
+@Component
+public class AppContextListener implements ApplicationListener<ContextRefreshedEvent> {
 
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
+    public void onApplicationEvent(ContextRefreshedEvent event) {
         DatabaseConfig db = DatabaseConfig.getInstance();
         db.runMigrations();
         // Warm up singleton chain: service -> repository -> DatabaseConfig
@@ -21,11 +22,5 @@ public class AppContextListener implements ServletContextListener {
         com.petclinic.service.PetService.getInstance();
         com.petclinic.service.VetService.getInstance();
         com.petclinic.service.VisitService.getInstance();
-        sce.getServletContext().log("PetClinic started – migrations applied.");
-    }
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        sce.getServletContext().log("PetClinic stopped.");
     }
 }
--- src/main/java/com/petclinic/spring/SpringContextListener.java
+++ src/main/java/com/petclinic/spring/SpringContextListener.java
@@ -3,11 +3,13 @@ package com.petclinic.spring;
 import com.petclinic.PetclinicApplication;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@WebListener
 public class SpringContextListener implements ServletContextListener {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringContextListener.class);
